6d69c013de5e479e5725176e29641b21b37ffbf9
jjc753 <jaydenjihochoi@gmail.com>
2026-04-18 15:26:50 -0700

    Update verdent.jsx

--- verdent.jsx
+++ verdent.jsx
@@ -11,22 +11,23 @@ function Main(){
     // p -> unsustainable (purple)
 
 
-    let zHeight = 3;
-    let width = 3;
-    let height = 3;
+    let zHeight = 4;
+    let width = 4;
+    let height = 4;
 
-    let sWidth = 3;
-    let sHeight = 3;
-    
+    let sWidth = 4;
+    let sHeight = 4;
+
     let [TS, setTS] = useState(new Array(width * height * zHeight * sWidth * sHeight).fill('n'))
 
 
     let [sTile, setSTile] = useState({x: 0, y: 0, z: 0,});
     let twoRender = [];
     for (let i = 0; i < sWidth * sHeight; ++i){
+
         if (i % (width * 2) === 0 ) { twoRender.push(<div style={{width: "100%", height: "100%", /*border: "1px solid black", backgroundColor: "red"*/}}></div>); }
-        twoRender.push(<div style={{width: "100%", height: "100%", border: "4px outset black", backgroundColor: "dodgerblue", gridColumn: "span 2"}}>{TS[sTile.z * sWidth * sHeight * width * height + Tile.y * sWidth * sHeight * width + sTile.x * sWidth * sHeight + i]}</div>);
-        if (i % (width * 2) === width * 2 -1 ) {  twoRender.push(<div style={{width: "100%", height: "100%", /*border: "1px solid black", backgroundColor: "red"*/}}></div>); }
+        twoRender.push(<div style={{width: "100%", height: "100%", border: "1px solid black", backgroundColor: "lightblue", gridColumn: "span 2"}}>{TS[sTile.z * sWidth * sHeight * width * height + sTile.y * sWidth * sHeight * width + sTile.x * sWidth * sHeight + i]}</div>);
+        if (i % (width * 2) === width * 2 - 1 ) {  twoRender.push(<div style={{width: "100%", height: "100%", /*border: "1px solid black", backgroundColor: "red"*/}}></div>); }
     }
 
     let toRender = [];
@@ -41,8 +42,8 @@ function Main(){
             transform: "rotate(30deg)",
             backgroundColor: "lightgray",
             display: "grid",
-            gridTemplateColumns: `repeat(${width}, 100px`,
-            gridTemplateRows: `repeat(${width}, 1fr`,
+            gridTemplateColumns: `repeat(${width}, 100px)`,
+            gridTemplateRows: `repeat(${width}, 1fr)`,
             border: "1px solid black",
             width: "300px",
             margin: 90,
@@ -63,7 +64,7 @@ function Main(){
                 {toRender}
             </div>
             <div style={{height: "100%", width: "600px", border: "1px solid teal", display:"flex", justifyContent: "center", alignItems: "center"}}>
-                <div style={{backgroundColor: "pink", width: 300, height: 300, display: "grid", gridTemplateColumns: `repeat ${width}, 1fr`, gridTemplateRows: `repeat ${height}, 1fr`}}>
+                <div style={{padding: 10, gap: 5, backgroundColor: "dodgerblue", width: 300, height: 300, display: "grid", gridTemplateColumns: `repeat(${sWidth * 2 + 1}, 1fr)`, gridTemplateRows: `repeat(${sHeight}, 1fr)`}}>
                     {twoRender}
                 </div>
             </div>
