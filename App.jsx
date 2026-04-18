430d2e0fa46deb37cb46af4993e0e635fc91e43d
Julian Park <105089696+31parkjulianTehCoder@users.noreply.github.com>
2026-04-19 03:54:30 +0900

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,26 +1,27 @@
-let [verdentGamePageMode, setVerdentGamePageMode] = useState("Card");
-
-function Card() {
+function App() {
+  let [gameStarted, setStarted] = useState(false);
   return (
     <>
       <h1>Verdent: Sustainable City Builder</h1>
-      <p>
-        Build a sustainable city and see how environmentally friendly it can be.
-      </p>
-      <button onClick = {() => {
-        console.log("Changing")
-          setVerdentGamePageMode("Game");
-      }}>Start</button>
-    </>
-  );
-}
-
-function App() {
-  return(
-    <>
-      <h1>Verdent: Sustainable City Builder</h1>
-      {verdentGamePageMode === "Game" ? <Main /> : <Card />}
-      <CDNMap coordinates = {[0,0]} />
+      {gameStarted ? (
+        <Main />
+      ) : (
+        <>
+          <h1>Verdent: Sustainable City Builder</h1>
+          <p>
+            Build a sustainable city and see how environmentally friendly it can
+            be.
+          </p>
+          <button
+            onClick={() => {
+              setStarted(true);
+            }}
+          >
+            Start
+          </button>
+        </>
+      )}
+      <CDNMap coordinates={[0, 0]} />
     </>
   );
 }
