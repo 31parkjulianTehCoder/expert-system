125947498f77d04dfc58a06ebfa0ff9d60108c35
Julian Park <105089696+31parkjulianTehCoder@users.noreply.github.com>
2026-04-19 07:24:46 +0900

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,11 +1,12 @@
 function App() {
   let [gameStarted, setStarted] = useState(false);
+  let [competitive, setCompetitive] = useState(false)
   let [tempx, settempx] = useState(0);
   let [tempy, settempy] = useState(0);
   return (
     <>
       {gameStarted ? (
-        <Main />
+        competitive ? <Backup /> : <Main />
       ) : (
         <div className = "default" style = {{margin: "5px", padding: "5px"}}>
           <h1 style = {{margin: "5px", padding: "5px"}}>Verdent: Sustainable City Builder</h1>
@@ -13,14 +14,23 @@ function App() {
             Build a sustainable city and see how environmentally friendly it can
             be.
           </p>
-          <p style = {{margin: "5px", padding: "5px", color: "green"}}>Easy Mode is enabled. You'll have less objectives and a simpler editor.</p>
           <button
             style = {{margin: "5px", padding: "5px"}}
             onClick={() => {
+              setCompetitive(false);
               setStarted(true);
             }}
           >
-            Start
+            Start Easy Mode
+          </button>
+          <button
+            style = {{margin: "5px", padding: "5px"}}
+            onClick={() => {
+              setCompetitive(true);
+              setStarted(true);
+            }}
+          >
+            Start Competitive Mode
           </button>
           <a href = "./titlescreen.html">Back to title</a>
         </div>
